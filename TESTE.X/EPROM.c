8fb56a78efa4ae8a44f6c22f22fe76935a0c06e4
Andre-MR-Pereira <andregdpereira@gmail.com>
2021-12-28 16:48:17 +0000

    Eprom fix

--- TESTE.X/EPROM.c
+++ TESTE.X/EPROM.c
@@ -1,28 +1,30 @@
+#include <stdint.h>
 #include <xc.h>
 #include "EPROM.h"
-#include "./mcc_generated_files/EEPROM2_app.h"
+#include "mcc_generated_files/EEPROM2_app.h"
+#include "stdio.h"
 
-uint32_t operADDR(uint8_t operation){
-    uint32_t dataAddr;
+uint16_t operADDR(uint8_t operation){
+    uint16_t dataAddr;
     switch (operation){
         case OPER_MIN_TEMP:     //NOTE Temperature minimum
-            dataAddr = 0x10AB01;
+            dataAddr = 0xAB01;
             break;
 
         case OPER_MAX_TEMP:     //NOTE Temperature maximum
-            dataAddr = 0x10AB07;
+            dataAddr = 0xAB07;
             break;
 
         case OPER_MIN_LUM:     //NOTE Luminosity minimum
-            dataAddr = 0x10AB0D;
+            dataAddr = 0xAB0D;
             break;
 
         case OPER_MAX_LUM:     //NOTE Luminosity maximum
-            dataAddr = 0x10AB13;
+            dataAddr = 0xAB13;
             break;
 
         default:
-            dataAddr = 0x10BB13;
+            dataAddr = 0xBB13;
             break;
 
     }
@@ -30,54 +32,55 @@ uint32_t operADDR(uint8_t operation){
 }
 
 void initializeEPROM(){
-    uint8_t writeBuffer[] = {MAGIC_WORD,NREG_EPROM, NR_EPROM, WI_EPROM, RI_EPROM,PMON_EPROM,TALA_EPROM,ALAT_EPROM,ALAL_EPROM,ALAF_EPROM,CLKH_EPROM,CLKM_EPROM,CHECKSUM};
-
-    EEPROM2_WriteBlock(writeBuffer,13,INITIALIZATION_ADDRESS);
+    DATAEE_WriteByte(INITIALIZATION_ADDRESS, MAGIC_WORD);
+    DATAEE_WriteByte(INITIALIZATION_ADDRESS + 1, NREG_EPROM);
+    DATAEE_WriteByte(INITIALIZATION_ADDRESS + 2, NR_EPROM);
+    DATAEE_WriteByte(INITIALIZATION_ADDRESS + 3, WI_EPROM);
+    DATAEE_WriteByte(INITIALIZATION_ADDRESS + 4, RI_EPROM);
+    DATAEE_WriteByte(INITIALIZATION_ADDRESS + 5, PMON_EPROM);
+    DATAEE_WriteByte(INITIALIZATION_ADDRESS + 6, TALA_EPROM);
+    DATAEE_WriteByte(INITIALIZATION_ADDRESS + 7, ALAT_EPROM);
+    DATAEE_WriteByte(INITIALIZATION_ADDRESS + 8, ALAL_EPROM);
+    DATAEE_WriteByte(INITIALIZATION_ADDRESS + 9, ALAF_EPROM);
+    DATAEE_WriteByte(INITIALIZATION_ADDRESS + 10, CLKH_EPROM);
+    DATAEE_WriteByte(INITIALIZATION_ADDRESS + 11, CLKM_EPROM);
+    DATAEE_WriteByte(INITIALIZATION_ADDRESS + 12, CHECKSUM);
 }
 
 void storeEPROMBuild(uint8_t hours,uint8_t minutes,uint8_t seconds,uint8_t temperature,uint8_t luminosity,uint8_t operation){
-    uint32_t dataAddr;
+    uint16_t dataAddr;
 
     dataAddr = operADDR(operation);
-    uint8_t writeBuffer[] = {hours, minutes, seconds, temperature,luminosity};
-
-    EEPROM2_WriteBlock(writeBuffer,5,dataAddr);
-}
-
-void fetchEPROM(uint8_t operation,uint8_t* readBuffer){
-    uint32_t dataAddr;
-    dataAddr = operADDR(operation);
-    EEPROM2_ReadBlock(readBuffer,5,dataAddr);
-}
-
-void fetchEPROMInitialization(uint8_t* readBuffer){
-    EEPROM2_ReadBlock(readBuffer,13,INITIALIZATION_ADDRESS);
+    DATAEE_WriteByte(dataAddr, hours);
+    DATAEE_WriteByte(dataAddr + 1, minutes);
+    DATAEE_WriteByte(dataAddr + 2, seconds);
+    DATAEE_WriteByte(dataAddr + 3, temperature);
+    DATAEE_WriteByte(dataAddr + 4, luminosity);
 }
 
 void parseEPROMReading(uint8_t* hours,uint8_t* minutes,uint8_t* seconds,uint8_t* temperature,uint8_t* luminosity,uint8_t operation){
-    uint8_t readBuffer[5];
-    fetchEPROM(operation,readBuffer);
-    *hours = readBuffer[0];
-    *minutes = readBuffer[1];
-    *seconds = readBuffer[2];
-    *temperature = readBuffer[3];
-    *luminosity = readBuffer[4];
+    uint16_t dataAddr;
+    
+    dataAddr = operADDR(operation);
+    *hours = DATAEE_ReadByte(dataAddr);
+    *minutes = DATAEE_ReadByte(dataAddr + 1);
+    *seconds = DATAEE_ReadByte(dataAddr + 2);
+    *temperature = DATAEE_ReadByte(dataAddr + 3);
+    *luminosity = DATAEE_ReadByte(dataAddr + 4);
 }
 
 void parseEPROMInitialization(uint8_t* magic_word,uint8_t* NREG,uint8_t* NR,uint8_t* WI,uint8_t* RI,uint8_t* PMON,uint8_t* TALA,uint8_t* ALAT,uint8_t* ALAL,uint8_t* ALAF,uint8_t* CLKH,uint8_t* CLKM,uint8_t* checksum){
-    uint8_t readBuffer[13];
-    fetchEPROMInitialization(readBuffer);
-    *magic_word = readBuffer[0];
-    *NREG = readBuffer[1];
-    *NR = readBuffer[2];
-    *WI = readBuffer[3];
-    *RI = readBuffer[4];
-    *PMON = readBuffer[5];
-    *TALA = readBuffer[6];
-    *ALAT = readBuffer[7];
-    *ALAL = readBuffer[8];
-    *ALAF = readBuffer[9];
-    *CLKH = readBuffer[10];
-    *CLKM = readBuffer[11];
-    *checksum = readBuffer[12];
+    *magic_word = DATAEE_ReadByte(INITIALIZATION_ADDRESS);
+    *NREG = DATAEE_ReadByte(INITIALIZATION_ADDRESS + 1);
+    *NR = DATAEE_ReadByte(INITIALIZATION_ADDRESS + 2);
+    *WI = DATAEE_ReadByte(INITIALIZATION_ADDRESS + 3);
+    *RI = DATAEE_ReadByte(INITIALIZATION_ADDRESS + 4);
+    *PMON = DATAEE_ReadByte(INITIALIZATION_ADDRESS + 5);
+    *TALA = DATAEE_ReadByte(INITIALIZATION_ADDRESS + 6);
+    *ALAT = DATAEE_ReadByte(INITIALIZATION_ADDRESS + 7);
+    *ALAL = DATAEE_ReadByte(INITIALIZATION_ADDRESS + 8);
+    *ALAF = DATAEE_ReadByte(INITIALIZATION_ADDRESS + 9);
+    *CLKH = rDATAEE_ReadByte(INITIALIZATION_ADDRESS + 10);
+    *CLKM = rDATAEE_ReadByte(INITIALIZATION_ADDRESS + 11);
+    *checksum = rDATAEE_ReadByte(INITIALIZATION_ADDRESS + 12);
 }
--- TESTE.X/EPROM.h
+++ TESTE.X/EPROM.h
@@ -1,7 +1,7 @@
 #ifndef __EPROM_H
 #define __EPROM_H
 
-#define INITIALIZATION_ADDRESS 0x10AA01
+#define INITIALIZATION_ADDRESS 0xAA01
 
 #define MAGIC_WORD 0xAA 
 #define CHECKSUM 0XE3
@@ -22,11 +22,9 @@
 #define CLKH_EPROM 12    
 #define CLKM_EPROM 0    
 
-uint32_t operADDR(uint8_t operation);
+uint16_t operADDR(uint8_t operation);
 void initializeEPROM();
 void storeEPROMBuild(uint8_t hours,uint8_t minutes,uint8_t seconds,uint8_t temperature,uint8_t luminosity,uint8_t operation);
-void fetchEPROM(uint8_t operation,uint8_t* readBuffer);
-void fetchEPROMInitialization(uint8_t* readBuffer);
 void parseEPROMReading(uint8_t* hours,uint8_t* minutes,uint8_t* seconds,uint8_t* temperature,uint8_t* luminosity,uint8_t operation);
 void parseEPROMInitialization(uint8_t* magic_word,uint8_t* NREG,uint8_t* NR,uint8_t* WI,uint8_t* RI,uint8_t* PMON,uint8_t* TALA,uint8_t* ALAT,uint8_t* ALAL,uint8_t* ALAF,uint8_t* CLKH,uint8_t* CLKM,uint8_t* checksum);
 
