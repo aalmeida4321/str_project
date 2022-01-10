bc89c96eeb4f55b883c0b6c9b8f855657eaf6497
aalmeida4321 <71839343+aalmeida4321@users.noreply.github.com>
2022-01-10 01:22:27 +0000

    Registos e Ring Buffer

--- TESTE.X/main.c
+++ TESTE.X/main.c
@@ -54,8 +54,15 @@
 #define LUM_LVL_01  0xFF
 #define LUM_LVL_12  0x1FE
 #define LUM_LVL_23  0x2FD
-#define START_WRITE_RING_BUFFER_ADDR 0xFF //TODO
-#define START_READ_RING_BUFFER_ADDR 0xFF //TODO
+#define START_RING_BUFFER_ADDR 0x7000
+#define COUNTER_ADDR 0x7050
+#define LAST_TEMP 0x7051
+#define LAST_LUX 0x7052
+#define HIGH_TEMP_REG 0x7053
+#define LOW_TEMP_REG 0x7054
+#define HIGH_LUX_REG 0x7055
+#define LOW_LUX_REG 0x7056
+
 
 
 void S1(void){
@@ -65,6 +72,50 @@ void S1(void){
 }
 
 void timerInterrupt(void){
+    
+    uint8_t counter = DATAEE_ReadByte(COUNTER_ADDR);
+    
+    //os ints nao sao necessarios mas ja nao tava a percerber nada do que estava a ver
+    uint8_t last_temperature = DATAEE_ReadByte(LAST_TEMP);
+    uint8_t last_luminosity = DATAEE_ReadByte(LAST_LUX);
+    uint8_t max_temperature = DATAEE_ReadByte(HIGH_TEMP_REG);
+    uint8_t min_temperature = DATAEE_ReadByte(LOW_TEMP_REG);
+    uint8_t max_luminosity = DATAEE_ReadByte(HIGH_LUX_REG);
+    uint8_t min_luminosity = DATAEE_ReadByte(LOW_LUX_REG);
+   
+    if(counter == 5){
+    
+        if(last_temperature > max_temperature){
+            
+            DATAEE_WriteByte(HIGH_TEMP_REG, last_temperature);
+            //alarme
+            
+        }else if(last_temperature < min_temperature){
+        
+            DATAEE_WriteByte(LOW_TEMP_REG, last_temperature); 
+            //alarme
+        }
+        if(last_luminosity > max_luminosity){
+            
+            DATAEE_WriteByte(HIGH_LUX_REG, last_luminosity);
+            //alarme
+            
+        }else if(last_luminosity < min_luminosity){
+        
+            DATAEE_WriteByte(LOW_LUX_REG, last_luminosity);
+            //alarme
+        } 
+    }
+ 
+    if(counter == 5){
+    
+        counter == 1;
+    }else{      
+        counter ++;
+    }
+    
+    DATAEE_WriteByte(COUNTER_ADDR, counter);
+    
     if(IO_RA7_GetValue()==LOW){
         IO_RA7_SetHigh();
     }else{
@@ -107,20 +158,68 @@ do{
 	return value;
 }
 
-uint8_t readRingBuffer(uint16_t dataAddr){
-    return 0x55;
+//iniciar os 4 registos com máximos e minimos
+//not sure com que valores inicializar os registos
+void initializeREG(){
+    
+        uint8_t aux = 1;
+    
+    DATAEE_WriteByte(COUNTER_ADDR, aux);
+    DATAEE_WriteByte(HIGH_TEMP_REG,(uint8_t) OPER_MAX_TEMP);
+    DATAEE_WriteByte(LOW_TEMP_REG,(uint8_t) OPER_MIN_TEMP);
+    DATAEE_WriteByte(HIGH_LUX_REG + 2,(uint8_t) OPER_MAX_LUM);
+    DATAEE_WriteByte(LOW_LUX_REG + 3,(uint8_t) OPER_MIN_LUM);
+
 }
 
-uint8_t writeRingBuffer(uint16_t dataAddr,uint8_t data){
-    return 0x55;
+//retorna o writeRingBufferAddr atualizado para a proxima escrita
+//ordem de escrita é: horas -> minutos -> segundos -> temperatura -> luminosidade -> horas -> (...)
+//writeRingBufferAddr = writeRingBuffer(...)
+uint16_t writeRingBuffer(uint16_t writeRingBufferAddr,uint8_t hours, uint8_t minutes, uint8_t seconds, uint8_t temperature, uint8_t luminosity){
+    
+    
+    uint8_t prev_temperature;
+    uint8_t prev_luminosity;
+    
+    if(writeRingBufferAddr == (START_RING_BUFFER_ADDR)){
+        
+        prev_temperature = DATAEE_ReadByte(writeRingBufferAddr + 23);
+        prev_luminosity = DATAEE_ReadByte(writeRingBufferAddr + 24);
+        
+    }else{
+        
+        prev_temperature = DATAEE_ReadByte(writeRingBufferAddr -2);
+        prev_luminosity = DATAEE_ReadByte(writeRingBufferAddr -1);
+    }
+    
+    if(prev_temperature != temperature || prev_luminosity != luminosity){
+        
+        DATAEE_WriteByte(writeRingBufferAddr, hours);
+        DATAEE_WriteByte(writeRingBufferAddr + 1, minutes);
+        DATAEE_WriteByte(writeRingBufferAddr + 2, seconds);
+        DATAEE_WriteByte(writeRingBufferAddr + 3, temperature);
+        DATAEE_WriteByte(writeRingBufferAddr + 4, luminosity);   
+        
+        DATAEE_WriteByte(LAST_TEMP, temperature);
+        DATAEE_WriteByte(LAST_LUX, luminosity);
+    }
+    
+    if(writeRingBufferAddr == (START_RING_BUFFER_ADDR + 20)){
+        
+        writeRingBufferAddr = START_RING_BUFFER_ADDR;
+        
+    }else{
+        
+        writeRingBufferAddr =  writeRingBufferAddr* + 5;
+    }
+    return writeRingBufferAddr;
 }
 
 void main(void)
 {
     unsigned char c;
     char buf[17];
-    uint16_t writeRingBufferAddr = START_WRITE_RING_BUFFER_ADDR; 
-    uint16_t readRingBufferAddr = START_READ_RING_BUFFER_ADDR; 
+
     // initialize the device
     SYSTEM_Initialize();
 
@@ -151,8 +250,15 @@ void main(void)
     //LCDinit();
 
     initializeEPROM();
+    
+    initializeREG();
 
     storeEPROMBuild(0x55,0x50,0x45,0x40,0x35,OPER_MAX_TEMP);
+    
+
+            
+    uint16_t writeRingBufferAddr = START_RING_BUFFER_ADDR; 
+    uint16_t readRingBufferAddr = START_RING_BUFFER_ADDR; 
     
     uint8_t hours,minutes,seconds,temperature,luminosity;
     uint8_t magic_word,NREG,NR,WI,RI,PMON,TALA,ALAT,ALAL,ALAF,CLKH,CLKM,checksum;
